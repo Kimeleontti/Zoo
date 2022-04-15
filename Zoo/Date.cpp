06a0226cf9144b92d60940dedba26322ddb5d582
Kimmo Hänninen <Kimmo.E.Hanninen@student.lab.fi>
2022-04-15 18:56:28 +0300

    initial

--- Zoo/Date.cpp
+++ Zoo/Date.cpp
@@ -1,1 +1,17 @@
 #include "Date.h"
+string Date::Date()
+{
+	// now we formulate person contents using c-language functions
+	const size_t bufsize = 1024;
+	char buf[bufsize];
+	// 1) sprintf() is unsafe (buffer overflow!) --> project properties | c/c++ | general | sdl check off
+	//sprintf(buf,"%s %s %s", this->getFname().c_str(), this->getLname().c_str(),
+	//	this->isFemale() ? "female" : "miesoletettu");
+	// 2) sprintf_s() is recommended safe version (but with problem: not found in every libs)
+	//sprintf_s(buf, "%s %s %s", this->getFname().c_str(), this->getLname().c_str(),
+	//	this->isFemale() ? "female" : "miesoletettu");
+	// 3) snprintf() is safe and is found in every libs
+	snprintf(buf, bufsize, "%s %s %s", this->getFname().c_str(), this->getLname().c_str(),
+		this->isFemale() ? "female" : "male?");
+	return buf; // compiler: return string(buf);
+}
--- Zoo/Date.h
+++ Zoo/Date.h
@@ -1,6 +1,19 @@
 #pragma once
+#include <string>
+using namespace std;
+
 class Date
 {
+	private:
+	// string is standard c++ string but it needs #include <string> and using namespace std;
+		int Y;
+		int M;
+		int D;
 
+public:
+	Date();
+	Date(int y, int m, int d);
+	string ToString();
+	bool setValues(int y, int m, int d);
 };
 
